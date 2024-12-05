7bec22f582b9771dec2437f690bc7c0082c0d0ab
Dan Walker <dwalker109@gmail.com>
2024-12-05 09:24:26 +0000

    WIIIP

--- days/day05/src/main.rs
+++ days/day05/src/main.rs
@@ -11,28 +11,64 @@ fn main() {
 fn part1(input: &'static str) -> Answer {
     let (rules, updates) = parse(input);
 
-    let correct = updates.iter().filter(|u| {
-        for (i, p) in u.iter().enumerate() {
-            for x in &u[..i] {
-                if rules.contains(&(*p, *x)) {
-                    return false;
-                }
-            }
-            for x in &u[i..] {
-                if rules.contains(&(*x, *p)) {
-                    return false;
-                }
-            }
-        }
-
-        true
-    });
-
-    correct.map(|u| u[(u.len() - 1) / 2]).sum()
+    updates
+        .iter()
+        .filter(|u| is_correct(u, &rules))
+        .map(|u| u[(u.len() - 1) / 2])
+        .sum()
 }
 
 fn part2(input: &'static str) -> Answer {
-    todo!();
+    let (rules, updates) = parse(input);
+
+    let incorrect = updates
+        .iter()
+        .filter(|u| !is_correct(u, &rules))
+        .collect::<Vec<_>>();
+
+    dbg!(&incorrect);
+
+    let mut results = Vec::new();
+    for &update in incorrect.iter() {
+        let mut curr = update.clone();
+        let mut working = update.clone();
+
+        loop {
+            let mut stable = true;
+
+            for (p_idx, p) in curr.iter().enumerate() {
+                for (x_idx, x) in curr[..p_idx].iter().enumerate() {
+                    if rules.contains(&(*p, *x)) {
+                        dbg!("Swapping 1", p_idx, x_idx);
+                        working.swap(p_idx, x_idx);
+                        dbg!(&working);
+                        continue;
+                        // stable = false;
+                    }
+                }
+                for (x_idx, x) in curr[p_idx..].iter().enumerate() {
+                    if rules.contains(&(*x, *p)) {
+                        dbg!("Swapping 2", p_idx, x_idx);
+                        working.swap(p_idx, x_idx);
+                        dbg!(&working);
+                        continue;
+                        // stable = false;
+                    }
+                }
+            }
+
+            if stable {
+                results.push(curr);
+                break;
+            } else {
+                curr = working.clone();
+            }
+        }
+    }
+
+    dbg!(&results);
+
+    todo!()
 }
 
 fn parse(input: &str) -> (HashSet<(usize, usize)>, Vec<Vec<usize>>) {
@@ -52,6 +88,23 @@ fn parse(input: &str) -> (HashSet<(usize, usize)>, Vec<Vec<usize>>) {
         .collect();
 
     (rules, updates)
+}
+
+fn is_correct(update: &[usize], rules: &HashSet<(usize, usize)>) -> bool {
+    for (i, p) in update.iter().enumerate() {
+        for x in &update[..i] {
+            if rules.contains(&(*p, *x)) {
+                return false;
+            }
+        }
+        for x in &update[i..] {
+            if rules.contains(&(*x, *p)) {
+                return false;
+            }
+        }
+    }
+
+    true
 }
 
 #[cfg(test)]
